e3c9ecd8d207401df065bab7eb9f396a5444931f
JoyceQuan <ytmpqqx@163.com>
2022-12-15 16:46:44 +0800

    12.15_Setting_Music_Sound

--- Classes/Scene/StartScene.cpp
+++ Classes/Scene/StartScene.cpp
@@ -8,17 +8,18 @@ bool StartScene::init() {
 		return false;
 	}
 
-	//背景音乐控制
+	//背景音乐控制（暂时没有考虑音量问题）
 	auto instance = CocosDenshion::SimpleAudioEngine::getInstance();
 	float volume;
-	if (!instance->isBackgroundMusicPlaying())
-		volume = 1.0f;
+	if (!instance->isBackgroundMusicPlaying()) {
+		//volume = 1.0f;
+	}
 	else {
-		volume = instance->getBackgroundMusicVolume();
+		//volume = instance->getBackgroundMusicVolume();
 		instance->stopBackgroundMusic(); //stop后无法恢复播放
 	}
 	instance->playBackgroundMusic("Music/Start.mp3", true);
-	instance->setBackgroundMusicVolume(volume);
+	//instance->setBackgroundMusicVolume(volume);
 	//auto backgroundAudioID = AudioEngine::play2d("Music/Start.mp3", true);//如果只这样简单设置的话，会导致每返回一次，就会开始播放背景音乐，导致音乐重叠。
 
 
